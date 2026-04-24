a15b66affc3a39550f58816ae8e44fe3470c970a
KaffeFilter-collab <148773655+KaffeFilter@users.noreply.github.com>
2026-04-24 20:54:29 +0200

    Img gonnna kill myself frfr I hate all of this
    Added bare bones animation
    Readded the lost things due to crashing

--- Assets/Scripts/Enemy Scripts/EnemyParentsClass.cs
+++ Assets/Scripts/Enemy Scripts/EnemyParentsClass.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public abstract class EnemyParentsClass : MonoBehaviour
 {
     private float currentHealth;
     [SerializeField] private float maxHealth;
     [SerializeField] private float damage;
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private Collider2D attackHitbox;
+    [SerializeField] public float movementSpeed;
+    [SerializeField] public Vector3 patrolStartPostion;
+    [SerializeField] public Vector3 patrolEndPosition;
+    public bool mustSwapPoints;
+    [SerializeField] public Collider2D attackHitbox;
+    [SerializeField] private Collider2D selfHitbox;
+    [SerializeField] private Collider2D enemyDetectionRadius;
 }
--- Assets/Scripts/Enemy Scripts/RatEnemy.cs
+++ Assets/Scripts/Enemy Scripts/RatEnemy.cs
@@ -1,16 +1,87 @@
+using System;
+using System.Collections;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
+
 
 public class RatEnemy : EnemyParentsClass
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public enum EnemyState
     {
-        
+        Patrolingstate,
+        Attackingstate,
+        Chasingstate
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private EnemyState currentState = EnemyState.Patrolingstate;
+
+    
+
+    private void FixedUpdate()
+    { 
+        switch (currentState)
+         {
+            case EnemyState.Patrolingstate:
+                StartPatrol();
+                break;
+            case EnemyState.Attackingstate:
+                StartAttack();
+                break;
+            case EnemyState.Chasingstate:
+                StartChasing();
+                break;
+         }
+    }
+
+    
+
+
+    #region EnemyControls internal
+
+    private void StartPatrol()
     {
-        
+        if (!mustSwapPoints)
+        {
+            transform.SetPositionAndRotation(Vector3.MoveTowards(transform.position, patrolEndPosition, movementSpeed),quaternion.identity);
+            if (transform.position == patrolEndPosition)
+            {
+                mustSwapPoints = true;
+            }
+        }
+        else
+        {
+            transform.SetPositionAndRotation(Vector3.MoveTowards(transform.position, patrolStartPostion, movementSpeed),quaternion.identity);
+            currentState = EnemyState.Attackingstate;
+            if (transform.position == patrolStartPostion)
+            {
+                mustSwapPoints = false;
+            }
+        }
+    }
+    private void StartAttack()
+    {
+        StartCoroutine(AttackCorutine());
+    }
+    
+    private void StartChasing()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+
+    IEnumerator AttackCorutine()
+    {
+        Debug.Log("In attack corutine");
+        yield return new  WaitForSeconds(2);
+        Debug.Log("First wait");
+        attackHitbox.enabled = true;
+        yield return new WaitForSeconds(1);
+        attackHitbox.enabled = false;
+        Debug.Log("Last wait");
+        currentState = EnemyState.Patrolingstate;
     }
 }
